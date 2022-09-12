d7c3471a62227d80940146021872283f6a03a089
Anastasia <104175772+AnnaRar@users.noreply.github.com>
2022-09-12 04:43:48 +0300

    forgot html and scripts

--- script.js
+++ script.js
@@ -29,7 +29,9 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date-time");
   let descriptionElement = document.querySelector("#description");
 
-  tempElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+
+  tempElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   windElement.innerHTML = `${response.data.wind.speed}m/s`;
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
@@ -49,7 +51,28 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("New York");
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
